47488e8dd9f31fc19019a1fb6e241649c67b5666
AdrianCasado-IETI <acasadoaguilera.cf@iesesteveterradas.cat>
2025-03-20 20:45:03 +0100

    finished

--- app/src/main/java/com/adriancasado/androidsensors/MainActivity.kt
+++ app/src/main/java/com/adriancasado/androidsensors/MainActivity.kt
@@ -7,11 +7,8 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.adriancasado.androidsensors.databinding.ActivityMainBinding
 import kotlin.math.abs
 
@@ -20,19 +17,18 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private lateinit var accelerometre: Sensor
     private lateinit var binding: ActivityMainBinding
-    private var counter: Int = 0
+
+    private var xTaps = 0
+    private var yTaps = 0
+    private var zTaps = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // Utilitzem bindings per facilitar accés als elements gràfics
-        // recorda activar-los a build.gradle.kts afegint:
-        // viewBinding { enable = true }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // escoltar variacions dels sensors
         sensorManager = getSystemService(
             Context.SENSOR_SERVICE) as SensorManager
         accelerometre = sensorManager.getDefaultSensor(
@@ -42,36 +38,31 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        // emprem les dades del sensor
         val x = event.values[0]
         val y = event.values[1]
         val z = event.values[2]
 
-        Log.i("TEST", z.toString())
-        if(abs(z) > 2) {
-            counter++
-            Toast.makeText(this, "Tap recibido", Toast.LENGTH_SHORT).show()
+        binding.xProgressBar.progress = abs(x * 10.0).toInt()
+        binding.yProgressBar.progress = abs(y * 10.0).toInt()
+        binding.zProgressBar.progress = abs(z * 10.0).toInt()
+
+        if(abs(x * 10.0).toInt() >= 20){
+            xTaps++
+            binding.xTapCounter.text = "$xTaps taps"
         }
 
-        binding.counter.text = counter.toString();
-        // 1g = 9,8 m/s² , què és un valor força alt.
-        // Al fer *10 ens acostem als 100, que és el valor màxim per defecte de la ProgressBar
-        binding.xProgressBar.progress = abs(x*10.0).toInt()
-        binding.yProgressBar.progress = abs(y*10.0).toInt()
-        binding.zProgressBar.progress = abs(z*10.0).toInt()
-    }
+        if(abs(y * 10.0).toInt() >= 20){
+            yTaps++
+            binding.yTapCounter.text = "$yTaps taps"
+        }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-
-        // ASEGURAR QUE NO CAMBIAN LOS VALORES AL GIRAR LA PANTALLA
-        // outState?.run {
-        //     putInt(STATE_SCORE, currentScore)
-        //     putInt(STATE_LEVEL, currentLevel)
-        // }
-        super.onSaveInstanceState(outState)
+        if(abs(z * 10.0).toInt() >= 20){
+            zTaps++
+            binding.zTapCounter.text = "$zTaps taps"
+        }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        // TODO("Not yet implemented")
+
     }
 }
